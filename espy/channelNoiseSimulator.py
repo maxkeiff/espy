4f95b5d3f6c48e13e6b7540e85d3669c2e6425b7
Jonas Heimann <Jonas_Heimann@web.de>
2019-12-18 11:29:28 +0100

    Refactoring to remove camelCase.

--- espy/channelNoiseSimulator.py
+++ espy/channelNoiseSimulator.py
@@ -1,11 +1,11 @@
 import numpy
-class channelNoiseSimulator:
+class channel_noise_simulator:
     """Class to hold usefull funktions to simulate noise in a channel"""
 
     def __init__(self):
         return
     #_____________create bits___________________
-    def createRandomBitsList (self, len):
+    def create_random_bits_list (self, len):
         """create a random len bits long bitstring """
 
         bits = []
@@ -14,7 +14,7 @@ class channelNoiseSimulator:
 
         return bits
 
-    def createRandomBitsString (self, len):
+    def create_random_bits_string (self, len):
         """create a random len bits long string """
 
         bits = ""
@@ -23,97 +23,97 @@ class channelNoiseSimulator:
 
         return bits
     #_____________Randoise bits______________________
-    def randomiseBitsList (self, bits, probability ):
+    def randomise_bits_list (self, bits, probability ):
         """A function to simply flip bits with the given probability
             ARGS: a list of bits, the probability for an error[0-1]
             RETURN: a list of bits
         """
 
-        newBits = []
+        new_bits = []
         for b in bits:
             if (probability > numpy.random.random()):#roll random numbers
-                newBits.append((b+1)%2)#turn 0 to 1 and 1 to 0
+                new_bits.append((b+1)%2)#turn 0 to 1 and 1 to 0
             else:
-                newBits.append(b)
+                new_bits.append(b)
 
-        return newBits
+        return new_bits
 
-    def randomiseBitsString (self, bits, probability ):
+    def randomise_bits_string (self, bits, probability ):
         """A function to simply flip bits with the given probability
             ARGS: a list of bits, the probability for an error[0-1]
             Return: a string full of bits
         """
 
-        newBits = ""
+        new_bits = ""
         for b in bits:
             if (probability > numpy.random.random()):#roll random numbers
-                newBits+=str((int(b)+1)%2)#turn 0 to 1 and 1 to 0
+                new_bits+=str((int(b)+1)%2)#turn 0 to 1 and 1 to 0
             else:
-                newBits+=b
+                new_bits+=b
 
-        return newBits
+        return new_bits
 
-    def randomiseBitsBurstList (self, bits, burstProbability  , errorRateInBurst = 0.9 ,maxBurstLength = 8):
+    def randomise_bits_burst_list (self, bits, burst_probability  , error_rate_in_burst = 0.9 ,max_burst_length = 8):
         """A function to simply flip bits with the given probability
             ARGS: a list of bits, the probability for an error[0-1], the maximum length of a burst error,the rate of errors within the bursterror[0-1]
             Return: list of bits with added burst erorrs
         """
 
-        newBits = []
+        new_bits = []
         i=0
         while (i < len(bits)):
-            if (burstProbability > numpy.random.random()):#roll random numbers
-                curentBurstLength = 0
-                burstLength = numpy.random.randint(1,high = maxBurstLength+1)
+            if (burst_probability > numpy.random.random()):#roll random numbers
+                curent_burst_length = 0
+                burst_length = numpy.random.randint(1,high = max_burst_length+1)
 
-                while (burstLength > curentBurstLength and i < len(bits)):#stop on burst end,#stop when bitstream ends (simulate one bursterror and adjust i)
-                    if (errorRateInBurst > numpy.random.random()):
-                        newBits.append((bits[i]+1)%2)#turn 0 to 1 and 1 to 0 randomly
+                while (burst_length > curent_burst_length and i < len(bits)):#stop on burst end,#stop when bitstream ends (simulate one bursterror and adjust i)
+                    if (error_rate-in_burst > numpy.random.random()):
+                        new_bits.append((bits[i]+1)%2)#turn 0 to 1 and 1 to 0 randomly
                     else:
-                        newBits.append(bits[i])
-                    curentBurstLength+=1
+                        new_bits.append(bits[i])
+                    curent_burst_length+=1
                     i+=1
             else:
-                newBits.append(bits[i])
+                new_bits.append(bits[i])
                 i+=1
 
 
-        return newBits
+        return new_bits
 
-    def randomiseBitsBurstString (self, bits, burstProbability  , errorRateInBurst = 0.9 ,maxBurstLength = 8):
+    def randomise_bits_burst_string (self, bits, burst_probability  , error_rate_in_burst = 0.9 ,max_burst_length = 8):
         """A function to simply flip bits with the given probability
             ARGS: a list of bits, the probability for an error[0-1], the maximum length of a burst error,the rate of errors within the bursterror[0-1]
             Return: String of bits with added burst erorrs
         """
 
-        newBits = ""
+        new_bits = ""
         i=0
         while (i < len(bits)):
-            if (burstProbability > numpy.random.random()):#roll random numbers
-                curentBurstLength = 0
-                burstLength = numpy.random.randint(1,high = maxBurstLength+1)
+            if (burst_probability > numpy.random.random()):#roll random numbers
+                curent_burst_length = 0
+                burst_length = numpy.random.randint(1,high = max_burst_length+1)
 
-                while (burstLength > curentBurstLength and i < len(bits)):#stop on burst end,#stop when bitstream ends (simulate one bursterror and adjust i)
-                    if (errorRateInBurst > numpy.random.random()):
-                        newBits+=str(((int(bits[i])+1)%2))#turn 0 to 1 and 1 to 0 randomly
+                while (burst_length > curent_burst_length and i < len(bits)):#stop on burst end,#stop when bitstream ends (simulate one bursterror and adjust i)
+                    if (error_rate_in_burst > numpy.random.random()):
+                        new_bits+=str(((int(bits[i])+1)%2))#turn 0 to 1 and 1 to 0 randomly
                     else:
-                        newBits+=str(bits[i])
-                    curentBurstLength+=1
+                        new_bits+=str(bits[i])
+                    curent_burst_length+=1
                     i+=1
             else:
-                newBits+=str(bits[i])
+                new_bits+=str(bits[i])
                 i+=1
 
 
-        return newBits
+        return new_bits
     #______________compare bits__________________________
-    def compareAndHighlightDifferences (self, bits1,bits2):
+    def compare_and_highlight_differences (self, bits1,bits2):
         """compare two bitlists and higlight the differences"""
         differences = []
         if (len(bits1) != len(bits2)):
             print("waning, different lengths detected. may result in higher errorrate")
-        minLength = min(len(bits1),len(bits2))
-        for i in range(minLength):
+        min_length = min(len(bits1),len(bits2))
+        for i in range(min_length):
             differences.append(1 if bits1[i]!=bits2[i] else 0)
         print("Differences found: " + str( differences.count(True)))
         return differences
@@ -121,13 +121,13 @@ class channelNoiseSimulator:
 
 
 
-#c=channelNoiseSimulator()
-#print c.randomiseBitsList([1,1,1,1,0,0,0,0,1],0.5)
-#print c.randomiseBitsString("1101110",0.5)
-#print c.compareAndHighlightDifferences([1,1,1,0,0,1,1,0,0,1,0,1,1,1],[0,1,1,0,0,1,1,1,1,1,0,1,0,1])
-#print c.createRandomBitsList(200)
-#rb= c.createRandomBitsString(200)
-#rr = c.randomiseBitsBurstString(rb,0.01,.8,16)
+#c=channel_noise_simulator()
+#print c.randomise_bits_list([1,1,1,1,0,0,0,0,1],0.5)
+#print c.randomise_bits_string("1101110",0.5)
+#print c.compare_and_highlight_differences([1,1,1,0,0,1,1,0,0,1,0,1,1,1],[0,1,1,0,0,1,1,1,1,1,0,1,0,1])
+#print c.create_random_bits_list(200)
+#rb= c.create_random_bits_string(200)
+#rr = c.randomise_bits_burst_string(rb,0.01,.8,16)
 
-#print c.compareAndHighlightDifferences("11110000","11001100")
+#print c.compare_and_highlight_differences("11110000","11001100")
 #"""
