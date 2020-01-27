f6cae639bb570ddfd66227c4db5fb745b3d88774
Jonas Heimann <Jonas_Heimann@web.de>
2020-01-27 10:51:30 +0100

    Naming

--- espy/setups/simulation_burst_error_crc.py
+++ espy/setups/simulation_burst_error_crc.py
@@ -3,7 +3,7 @@ from algorithms.crc import CRC_8
 from algorithms.channel_noise_simulator import channel_noise_simulator
 
 
-class SimpleErrorCRCSetup(ErrorSimulationSetup):
+class BurstErrorCRCSetup(ErrorSimulationSetup):
     def __init__(self, p_enter,p_leave, *args, **kwargs):
         """Initializes a Simulation Setup with only Bursterrors and probability p_enter to start bursting
          and probability p_leave to stop bursting and with CRC8"""
@@ -20,6 +20,6 @@ class SimpleErrorCRCSetup(ErrorSimulationSetup):
 
     def crc_check_and_remove(self, msg):
         return self.crc.crc_decode(msg)
-        
+
     def apply_channel(self, bitstring):
         return self.cns.randomise_bits_burst_string(bitstring,self.p_enter,self.p_leave)
