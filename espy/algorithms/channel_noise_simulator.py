075e79ce87e0dc61dac9c4a94abff176c1bd9490
Paul Kramer <pkramer1503@gmail.com>
2020-01-28 19:58:14 +0100

    Fix some bugs and add final presentation.

--- espy/algorithms/channel_noise_simulator.py
+++ espy/algorithms/channel_noise_simulator.py
@@ -57,54 +57,59 @@ class channel_noise_simulator:
 
         return new_bits
 
-    def randomise_bits_string_list(self,bits, probability):
-    """A function to simply flip bits with the given probability
-        ARGS: a list of bits, the probability for an error[0-1]
-        RETURN: a list of bits
-    """
+    def randomise_bits_string_list(self, bits, probability):
+        """A function to simply flip bits with the given probability
+            ARGS: a list of bits, the probability for an error[0-1]
+            RETURN: a list of bits
+        """
 
-    new_bits = []
-    for b in bits:
-        new_bit =""
-        for i in range(len(b)):
-            if probability > numpy.random.random():  # roll random numbers
-                new_bit += str((int(b[i]) + 1) % 2) # turn 0 to 1 and 1 to 0
-            else:
-                new_bit += str(b[i])
-        
-        new_bits.append(new_bit)
+        new_bits = []
+        for b in bits:
+            new_bit = ""
+            for i in range(len(b)):
+                if probability > numpy.random.random():  # roll random numbers
+                    new_bit += str((int(b[i]) + 1) % 2)  # turn 0 to 1 and 1 to 0
+                else:
+                    new_bit += str(b[i])
 
-    return new_bits
-    
-    def randomise_bits_burst_string_list(self,bits, burst_probability, error_rate_in_burst=0.9,):
-    """A function to simply flip bits with the given probability
-       ARGS: a String of bits, the probability for an error[0-1], the probability to leave the bursterror[0-1]
-        Return: String of bits with added burst error
-    """
+            new_bits.append(new_bit)
 
-    new_bits = []
-    currently_bursting = False
-    for b in bits:
-        i = 0
-        new_bits.append("")
-        while i < len(b):
-            if burst_probability > numpy.random.random():  # roll random numbers
-                currently_bursting=True
-                
-                while currently_bursting and i < len(b):  #stop when bitstream ends (simulate one bursterror and adjust i)
-                    if error_rate_in_burst > numpy.random.random():
-                        new_bits[len(new_bits)-1] += str(((int(b[i]) + 1) % 2))  # turn 0 to 1 and 1 to 0 randomly
-                    else:
-                        new_bits[len(new_bits)-1] += str(b[i])
-                        currently_bursting = False
+        return new_bits
+
+    def randomise_bits_burst_string_list(
+        self, bits, burst_probability, error_rate_in_burst=0.9,
+    ):
+        """A function to simply flip bits with the given probability
+        ARGS: a String of bits, the probability for an error[0-1], the probability to leave the bursterror[0-1]
+            Return: String of bits with added burst error
+        """
+
+        new_bits = []
+        currently_bursting = False
+        for b in bits:
+            i = 0
+            new_bits.append("")
+            while i < len(b):
+                if burst_probability > numpy.random.random():  # roll random numbers
+                    currently_bursting = True
+
+                    while currently_bursting and i < len(
+                        b
+                    ):  # stop when bitstream ends (simulate one bursterror and adjust i)
+                        if error_rate_in_burst > numpy.random.random():
+                            new_bits[len(new_bits) - 1] += str(
+                                ((int(b[i]) + 1) % 2)
+                            )  # turn 0 to 1 and 1 to 0 randomly
+                        else:
+                            new_bits[len(new_bits) - 1] += str(b[i])
+                            currently_bursting = False
+                        i += 1
+                else:
+                    new_bits[len(new_bits) - 1] += str(b[i])
                     i += 1
-            else:
-                new_bits[len(new_bits)-1] += str(b[i])
-                i += 1
 
-    return new_bits
+        return new_bits
 
-    
     def randomise_bits_burst_list(
         self, bits, burst_probability, error_rate_in_burst=0.9
     ):
@@ -117,9 +122,11 @@ class channel_noise_simulator:
         i = 0
         while i < len(bits):
             if burst_probability > numpy.random.random():  # roll random numbers
-                currently_bursting=True
+                currently_bursting = True
 
-                while currently_bursting and i < len(bits):  #stop when bitstream ends (simulate one bursterror and adjust i)
+                while currently_bursting and i < len(
+                    bits
+                ):  # stop when bitstream ends (simulate one bursterror and adjust i)
                     if error_rate_in_burst > numpy.random.random():
                         new_bits.append(
                             (bits[i] + 1) % 2
@@ -146,9 +153,11 @@ class channel_noise_simulator:
         i = 0
         while i < len(bits):
             if burst_probability > numpy.random.random():  # roll random numbers
-                currently_bursting=True
+                currently_bursting = True
 
-                while currently_bursting and i < len(bits):  #stop when bitstream ends (simulate one bursterror and adjust i)
+                while currently_bursting and i < len(
+                    bits
+                ):  # stop when bitstream ends (simulate one bursterror and adjust i)
                     if error_rate_in_burst > numpy.random.random():
                         new_bits += str(
                             ((int(bits[i]) + 1) % 2)
@@ -176,13 +185,13 @@ class channel_noise_simulator:
         return differences
 
 
-#c=channel_noise_simulator()
+# c=channel_noise_simulator()
 # print (c.randomise_bits_list([1,1,1,1,0,0,0,0,1],0.5))
 # print (c.randomise_bits_string("1101110",0.5))
 # print (c.compare_and_highlight_differences([1,1,1,0,0,1,1,0,0,1,0,1,1,1],[0,1,1,0,0,1,1,1,1,1,0,1,0,1]))
 # print (c.create_random_bits_list(200))
-#rb= c.create_random_bits_string(200)
-#rr = c.randomise_bits_burst_string(rb,0.01,.9)
+# rb= c.create_random_bits_string(200)
+# rr = c.randomise_bits_burst_string(rb,0.01,.9)
 
-#print (c.compare_and_highlight_differences(rb,rr))
+# print (c.compare_and_highlight_differences(rb,rr))
 # """
--- espy/setups/simulation_simple_error_viterbi.py
+++ espy/setups/simulation_simple_error_viterbi.py
@@ -1,21 +1,32 @@
 from simulation.error_setup import ErrorSimulationSetup
-from algorithms.cc_viterbi import *
+import algorithms.cc_viterbi as cc_viterbi
 from algorithms.channel_noise_simulator import channel_noise_simulator
+from algorithms.crc import CRC_8
 
 class SimpleErrorViterbiSetup(ErrorSimulationSetup):
-    def __init__(self, rate = 0.5, p, *args, **kwargs):
+    def __init__(self, p, rate = 0.5, *args, **kwargs):
         """Initializes a Simulation Setup with bit flip probability p"""
         self.p = p
-        self.cns = channel_noise_simulator
+        self.cns = channel_noise_simulator()
         self.rate = rate
-        super().__init__(*args, crc_size=0, **kwargs)
-
-    def encode_message(self, msg):
-        return cc_viterbi.encode_message(msg, self.rate)
-
-    def decode_message(self, msg):        
-        return cc_viterbi.decode_message(msg, self.rate)
+        self.crc = CRC_8()
+        super().__init__(*args, crc_size=8, **kwargs)
 
     def apply_channel(self, bitstring):
-        new_bitstring = cns.randomise_bits_string_list(bitstring,self.p)
+        new_bitstring = self.cns.randomise_bits_string_list(bits=bitstring, probability=self.p)
+        new_bitstring = "".join(new_bitstring)
         return new_bitstring
+
+    def crc_add(self, msg):
+        return self.crc.crc_encode(msg)
+
+    def crc_check_and_remove(self, msg):
+        return self.crc.crc_decode(msg)
+
+    def fec_encode(self, msg):
+        enc_list = cc_viterbi.encode_message(msg, self.rate)
+        return "".join(enc_list)
+
+    def fec_decode(self, msg):
+        
+        return cc_viterbi.decode_message(msg, self.rate)
