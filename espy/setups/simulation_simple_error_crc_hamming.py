2194d1fbfb208a2a13c2ea66d06b134ba094c436
Maximilian Keiff <maximilian.keiff@gmail.com>
2020-01-29 02:00:31 +0100

    Added Simple Error Simulation for Hamming Code

--- espy/setups/simulation_simple_error_crc_hamming.py
+++ espy/setups/simulation_simple_error_crc_hamming.py
@@ -1,15 +1,19 @@
+from algorithms.block_code import HammingCode
 from simulation.error_setup import ErrorSimulationSetup
 from algorithms.channel_noise_simulator import channel_noise_simulator
 from algorithms.crc import CRC_8
+from espy.algorithms.block_code import *
+
 
 class CRCHammingErrorSetup(ErrorSimulationSetup):
-    def __init__(self, p, rate = 0.5, *args, **kwargs):
+    def __init__(self, p, rate=0.5, *args, **kwargs):
         """Initializes a Simulation Setup with bit flip probability p"""
         self.p = p
         self.cns = channel_noise_simulator()
         self.rate = rate
         self.crc = CRC_8()
-        fec_size = 0 # TODO
+        fec_size = 3
+        self.hamming_code = HammingCode(3)
         super().__init__(*args, crc_size=8, fec_size=fec_size, **kwargs)
 
     def apply_channel(self, bitstring):
@@ -24,7 +28,7 @@ class CRCHammingErrorSetup(ErrorSimulationSetup):
         return self.crc.crc_decode(msg)
 
     def fec_encode(self, msg):
-        raise NotImplementedError
+        return ''.join(str(e) for e in self.hamming_code.encode(msg))
 
     def fec_decode(self, msg):
-        raise NotImplementedError
+        return ''.join(str(e) for e in self.hamming_code.maximum_likelihood_decode(msg))
